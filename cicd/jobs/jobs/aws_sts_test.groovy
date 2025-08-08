1b5095f8a45e70a7920f74262148968ac0769ba2
Umesh M. Gaikwad <gaikwadumesh123@gmail.com>
2025-08-09 03:49:15 +0530

    Update aws_sts_test.groovy

--- cicd/jobs/jobs/aws_sts_test.groovy
+++ cicd/jobs/jobs/aws_sts_test.groovy
@@ -1,24 +1,35 @@
 pipelineJob("aws-sts-test") {
-    description("Test AWS STS with credentials from AWS Secrets Manager")
+    description("Test AWS STS with credentials from AWS Secrets Manager using awscli podTemplate")
     keepDependencies(false)
     definition {
         cps {
             script("""
-                node {
-                  docker.image('amazon/aws-cli:2.15.20').inside {
-                    withCredentials([string(credentialsId: 'aws-temp-creds', variable: 'AWS_CREDS_JSON')]) {
-                      def creds = readJSON text: AWS_CREDS_JSON
-                      withEnv([
-                        "AWS_ACCESS_KEY_ID=\${creds.AWS_ACCESS_KEY_ID}",
-                        "AWS_SECRET_ACCESS_KEY=\${creds.AWS_SECRET_ACCESS_KEY}",
-                        "AWS_SESSION_TOKEN=\${creds.AWS_SESSION_TOKEN}",
-                        "AWS_REGION=us-west-2",
-                        "AWS_PAGER="
-                      ]) {
-                        sh 'aws sts get-caller-identity'
-                      }
+                pipeline {
+                    agent {
+                        label 'awscli'
                     }
-                  }
+                    
+                    stages {
+                        stage('AWS STS Test') {
+                            steps {
+                                container('aws') {
+                                    withCredentials([string(credentialsId: 'aws-temp-creds', variable: 'AWS_CREDS_JSON')]) {
+                                        script {
+                                            def creds = readJSON text: AWS_CREDS_JSON
+                                            withEnv([
+                                                "AWS_ACCESS_KEY_ID=\${creds.AWS_ACCESS_KEY_ID}",
+                                                "AWS_SECRET_ACCESS_KEY=\${creds.AWS_SECRET_ACCESS_KEY}",
+                                                "AWS_SESSION_TOKEN=\${creds.AWS_SESSION_TOKEN}",
+                                                "AWS_PAGER="
+                                            ]) {
+                                                sh 'aws sts get-caller-identity'
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
                 }
             """.stripIndent())
             sandbox(true)
