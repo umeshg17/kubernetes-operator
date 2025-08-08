1b7e3e339a3b288d0c026e7f13b3b6fba563f0e3
Umesh M. Gaikwad <gaikwadumesh123@gmail.com>
2025-08-08 09:44:41 +0530

    Create aws-sts-test.groovy

---
+++ cicd/jobs/jobs/aws-sts-test.groovy
@@ -0,0 +1,23 @@
+pipelineJob('aws-sts-test') {
+  definition {
+    cps {
+      script("""
+        node {
+          withCredentials([string(credentialsId: 'aws-temp-creds', variable: 'AWS_CREDS_JSON')]) {
+            def creds = readJSON text: AWS_CREDS_JSON
+            withEnv([
+              "AWS_ACCESS_KEY_ID=\${creds.AWS_ACCESS_KEY_ID}",
+              "AWS_SECRET_ACCESS_KEY=\${creds.AWS_SECRET_ACCESS_KEY}",
+              "AWS_SESSION_TOKEN=\${creds.AWS_SESSION_TOKEN}",
+              "AWS_REGION=us-west-2",
+              "AWS_PAGER="
+            ]) {
+              sh 'aws sts get-caller-identity'
+            }
+          }
+        }
+      """)
+      sandbox(false)
+    }
+  }
+}
