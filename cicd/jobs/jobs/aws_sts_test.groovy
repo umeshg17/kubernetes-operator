2ffcf8bdbf62beeda829adee45680d94254be5e6
Umesh M. Gaikwad <gaikwadumesh123@gmail.com>
2025-08-08 10:47:54 +0530

    Update aws_sts_test.groovy

--- cicd/jobs/jobs/aws_sts_test.groovy
+++ cicd/jobs/jobs/aws_sts_test.groovy
@@ -1,23 +1,26 @@
 pipelineJob("aws-sts-test") {
-	description()
-	keepDependencies(false)
-	definition {
-		cpsScm {
-"""
-        node {
-          withCredentials([string(credentialsId: 'aws-temp-creds', variable: 'AWS_CREDS_JSON')]) {
-            def creds = readJSON text: AWS_CREDS_JSON
-            withEnv([
-              "AWS_ACCESS_KEY_ID=\${creds.AWS_ACCESS_KEY_ID}",
-              "AWS_SECRET_ACCESS_KEY=\${creds.AWS_SECRET_ACCESS_KEY}",
-              "AWS_SESSION_TOKEN=\${creds.AWS_SESSION_TOKEN}",
-              "AWS_REGION=us-west-2",
-              "AWS_PAGER="
-            ]) {
-              sh 'aws sts get-caller-identity'
-            }
-          }
-        }"""		}
-	}
-	disabled(false)
+    description("Test AWS STS with credentials from AWS Secrets Manager")
+    keepDependencies(false)
+    definition {
+        cps {
+            script("""
+                node {
+                  withCredentials([string(credentialsId: 'aws-temp-creds', variable: 'AWS_CREDS_JSON')]) {
+                    def creds = readJSON text: AWS_CREDS_JSON
+                    withEnv([
+                      "AWS_ACCESS_KEY_ID=\${creds.AWS_ACCESS_KEY_ID}",
+                      "AWS_SECRET_ACCESS_KEY=\${creds.AWS_SECRET_ACCESS_KEY}",
+                      "AWS_SESSION_TOKEN=\${creds.AWS_SESSION_TOKEN}",
+                      "AWS_REGION=us-west-2",
+                      "AWS_PAGER="
+                    ]) {
+                      sh 'aws sts get-caller-identity'
+                    }
+                  }
+                }
+            """.stripIndent())
+            sandbox(true)
+        }
+    }
+    disabled(false)
 }
