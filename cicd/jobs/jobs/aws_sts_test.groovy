093fda1658148e1f2d45a5b46f166ba92fe2867a
Umesh M. Gaikwad <gaikwadumesh123@gmail.com>
2025-08-09 04:31:58 +0530

    Update aws_sts_test.groovy

--- cicd/jobs/jobs/aws_sts_test.groovy
+++ cicd/jobs/jobs/aws_sts_test.groovy
@@ -1,39 +1,36 @@
 pipelineJob("aws-sts-test") {
-    description("Test AWS STS with credentials from AWS Secrets Manager using awscli podTemplate")
-    keepDependencies(false)
-    definition {
-        cps {
-            script("""
-                pipeline {
-                    agent {
-                        label 'awscli'
-                    }
-                    
-                    stages {
-                        stage('AWS STS Test') {
-                            steps {
-                                container('aws') {
-                                    withCredentials([string(credentialsId: 'aws-temp-creds', variable: 'AWS_CREDS_JSON')]) {
-                                        script {
-                                            def creds = readJSON text: AWS_CREDS_JSON
-                                            withEnv([
-                                                "AWS_ACCESS_KEY_ID=\${creds.AWS_ACCESS_KEY_ID}",
-                                                "AWS_SECRET_ACCESS_KEY=\${creds.AWS_SECRET_ACCESS_KEY}",
-                                                "AWS_SESSION_TOKEN=\${creds.AWS_SESSION_TOKEN}",
-                                                "AWS_PAGER="
-                                            ]) {
-                                                sh 'aws sts get-caller-identity'
-                                            }
-                                        }
-                                    }
+	description("Test AWS STS with credentials from AWS Secrets Manager using awscli podTemplate")
+	keepDependencies(false)
+	definition {
+		cpsScm {
+"""
+    pipeline {
+        agent {
+            label 'awscli'
+        }
+
+        stages {
+            stage('AWS STS Test') {
+                steps {
+                    container('aws') {
+                        withCredentials([string(credentialsId: 'aws-temp-creds', variable: 'AWS_CREDS_JSON')]) {
+                            script {
+                                def creds = readJSON text: AWS_CREDS_JSON
+                                withEnv([
+                                    "AWS_ACCESS_KEY_ID=\${creds.AWS_ACCESS_KEY_ID}",
+                                    "AWS_SECRET_ACCESS_KEY=\${creds.AWS_SECRET_ACCESS_KEY}",
+                                    "AWS_SESSION_TOKEN=\${creds.AWS_SESSION_TOKEN}",
+                                    "AWS_PAGER="
+                                ]) {
+                                    sh 'aws sts get-caller-identity'
                                 }
                             }
                         }
                     }
                 }
-            """.stripIndent())
-            sandbox(true)
+            }
         }
-    }
-    disabled(false)
+    }"""		}
+	}
+	disabled(false)
 }
